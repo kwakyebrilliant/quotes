703e2ea76f2e502f33275d1bd62151449a437860
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-24 18:33:32 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -30,26 +30,30 @@ class _QuotesListState extends State<QuotesList> {
   Widget quoteTemplate(quote) {
     return Card(
       margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Column(
-        children: <Widget>[
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.grey[600],
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 6.0,
-          ),
-          Text(
-            quote.author,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.grey[800],
+            const SizedBox(
+              height: 6.0,
             ),
-          ),
-        ],
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
